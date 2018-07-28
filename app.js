49226576f10c05d744a0226820dd86ba9f8caac1
Brayan ODURO <brayan.oduro@gmail.com>
2018-07-28 23:50:39 +0200

    test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.render('index');
 })
 
 app.get('/api/persons', function (req, res) {
