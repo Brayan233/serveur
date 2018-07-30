0ba9ad6273d27634f13956071f2d99ac164b7c32
Brayan ODURO <brayan.oduro@gmail.com>
2018-07-30 03:53:24 +0200

    test

--- app.js
+++ app.js
@@ -22,8 +22,19 @@ var config =
         }
    }
 var connection = new Connection(config);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === "OPTIONS") 
+        res.send(200);
+    else 
+        next();
+}
 app.use(express.static(path.join(__dirname, 'views')));
 
+app.use(allowCrossDomain);
 
 
 app.use(function (req, res, next) {
