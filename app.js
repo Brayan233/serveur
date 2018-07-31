55d799ce47eaec9081dd37891f8bf7a77bbd1cc4
Brayan ODURO <brayan.oduro@gmail.com>
2018-07-31 10:02:31 +0200

    test

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get('/api/persons', function (req, res) {
 
 app.get('/php/azure',function(){
 
-    request('http://www.google.com', function (error, response, body) {
+    request('https://t2b-test.azure-api.net/api-test/api/persons', function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the HTML for the Google homepage.
