809e88b2d538c5933e03254f6abf1180ad7f7d5e
Brayan ODURO <brayan.oduro@gmail.com>
2018-07-31 10:40:30 +0200

    test

--- app.js
+++ app.js
@@ -61,10 +61,11 @@ app.get('/php/azure',function(req,res){
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the HTML for the Google homepage.
+      res.json(body)
+
     });
 
 
-    res.json(body)
 
 })
 
